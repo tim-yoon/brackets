8836271570ed8b37472d95d020af996359ad6ab6
Timur Ismayelov <fiske6r@mail.ru>
2021-03-15 15:49:05 +0500

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2) {
+        return false;
+    }
+    const sameBrackets = bracketsConfig
+        .filter((item) => item[0] === item[1])
+        .map((item) => item[0]);
+    const splitted = str.split("");
+    const opened = [];
+    let result = true;
+    for (let i = 0; i < splitted.length; i++) {
+        const element = splitted[i];
+        if (!opened.length) {
+            opened.push(element);
+            continue;
+        }
+        if (sameBrackets.includes(element)) {
+            if (opened[opened.length - 1] === element) {
+                opened.pop();
+                continue;
+            }
+            opened.push(element);
+            continue;
+        } else {
+            if (checkIfOpening(bracketsConfig, element)) {
+                opened.push(element);
+                continue;
+            }
+            const lastOpened = opened.pop();
+            if (getClosing(bracketsConfig, lastOpened) !== element) {
+                result = false;
+                break;
+            }
+        }
+    }
+    return result && !opened.length ? true : false;
+};
+function checkIfOpening(brackets, char) {
+    return !!brackets.filter((item) => item[0] === char).length;
+}
+function getClosing(brackets, char) {
+    const filtered = brackets.filter((item) => item[0] === char)[0];
+    return filtered ? filtered[1] : null;
 }
